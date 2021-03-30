4bbb261f54d8d100984dffbeb8e3fd5e32cf4de8
tiding123 <65895670+tiding123@users.noreply.github.com>
2021-03-30 15:40:13 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'download.jpeg') {
+      myImage.setAttribute('src','logo.jpeg');
+    } else {
+      myImage.setAttribute('src','download.jpeg');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'welcome to HUMPTY DUMPTY ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
